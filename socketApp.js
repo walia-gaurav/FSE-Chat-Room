40ec1c2e359d98c71932b1f067d9b57b38c74953
Gaurav Walia <walia-mac@Gauravs-MacBook-Air.local>
2015-08-26 18:47:47 -0700

    Added sync messaging

--- socketApp.js
+++ socketApp.js
@@ -2,25 +2,27 @@ var http = require('http');
 var express = require('express');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 
 var appServer = http.createServer(app);
 var io = require('socket.io')(appServer);
 
 app.use(express.static('public'));
 
+/* Code to show nodeJs image as a ping response */
 app.get('/ping', function(req, res) {
     res.sendFile(__dirname + '/ping.html');
 });
 
+/* Actual get URL to fetch CHAT window. */
 app.get('/chat', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 // accept POST request on the homepage
-app.post('/', function(req, res) {
-    res.send('Hey, I just received a POST request');
-});
+//app.post('/', function(req, res) {
+//    res.send('Hey, I just received a POST request');
+//});
 
 
 //io.on('connection', function(socket) {
@@ -28,20 +30,22 @@ app.post('/', function(req, res) {
  //   console.log("New connection from " + address);
 //});
 
+appServer.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
 io.on('connection', function (socket) {
-        console.log(socket.handshake.address + ' connected');
-    socket.on('disconnect', function () {
-        console.log(socket.handshake.address + ' disconnected');
-    });
+  console.log(socket.handshake.address + ' ' + socket.id + ' connected');
+  socket.on('disconnect', function () {
+    console.log(socket.handshake.address + ' ' + socket.id + ' disconnected');
+  });
 });
 
 
 io.on('connection', function(socket) {
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  socket.on('inputMessage', function(msg){
+    console.log('Received input: ' + msg + ' from ' + socket.id);
+    io.sockets.emit('inputMessage', msg);
   });
 });
 
-appServer.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
