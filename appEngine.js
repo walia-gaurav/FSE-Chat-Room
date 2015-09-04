f54ceb44235015da9685318feba32cb21bbe52c0
Gaurav Walia <email.gaurav.walia@gmail.com>
2015-09-03 22:12:31 -0700

    Mega clean up. Looks so good now!

--- appEngine.js
+++ appEngine.js
@@ -9,42 +9,34 @@ var io = require('socket.io')(appServer);
 
 app.use(express.static('public'));
 
-/* Code to show nodeJs image as a ping response */
 app.get('/ping', function(req, res) {
-    res.sendFile(__dirname + '/ping.html');
+    res.render('ping.jade');
 });
 
-/* Actual get URL to fetch CHAT window. */
 app.get('/chat', function(req, res) {
-    //res.sendFile(__dirname + '/index.html');
-
-    /* That's how I rock jade, man! */
-    res.render(__dirname + '/index.jade');
+    res.render('index.jade');
 });
 
-//Database declaration shit starts
 var fs = require("fs");
 var file = "test.db";
 var exists = fs.existsSync(file);
 
 if (!exists) {
     console.log("Creating DB file.");
-    fs.openSync(file, "w");
+    fs.openSync(file);
 }
 
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
-//Database declaration shit ends.
 
 appServer.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 io.on('connection', function(socket) {
+    
     console.log(socket.handshake.address + ' ' + socket.id + ' connected');
-
     db.serialize(function() {
-
         if (exists) {
             db.each("SELECT rowid AS id, thing FROM ChatHistory", function(err, row) {
                 socket.emit('inputMessage', row.thing);
@@ -55,22 +47,16 @@ io.on('connection', function(socket) {
     socket.on('disconnect', function() {
         console.log(socket.handshake.address + ' ' + socket.id + ' disconnected');
     });
-});
-
-io.on('connection', function(socket) {
 
     socket.on('inputMessage', function(msg) {
 
         console.log('Received input: ' + msg + ' from ' + socket.id);
-        //socket.broadcast.emit('inputMessage', msg); 
         io.emit('inputMessage', msg);
 
         db.serialize(function() {
-
             if (!exists) {
                 db.run("CREATE TABLE ChatHistory (thing TEXT)");
             }
-
             var stmt = db.prepare("INSERT INTO ChatHistory VALUES (?)");
             stmt.run(msg);
             stmt.finalize();
--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,33 @@
     var socket = io();
 
+    socket.on('inputMessage', function(msg) {
+        var emittedAtom = JSON.parse(msg.toString());
+        prepareChatBubble(emittedAtom);
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    });
+
+    $(function() {
+        renderLoginPage();
+    })
+
+    function renderLoginPage() {
+        $('#chatroom').hide();
+        $('#logout').hide();
+        $('#username').hide();
+        $('#login').show();
+        $('#m1').val('');
+    }
+
     function checkEmpty(input) {
         return (input.length === 0 || !input.trim());
-
     }
 
     function OnSubmit(from) {
         if (from == "fromChatWindow") {
+
             if (checkEmpty($('#m').val())) {
                 return;
             }
-
             var atom = {
                 what: $('#m').val(),
                 who: $('#m1').val(),
@@ -20,51 +37,37 @@
             $('#m').val('');
 
         } else if (from == "fromLogin") {
+
             if (checkEmpty($('#m1').val())) {
                 return;
             }
-
             socket.connect();
-            $('#chatroom').show();
-            $('#logout').show();
-            $('#username').show();
-            $('#username').text('Hi ' + $('#m1').val() + '!')
-            $('#login').hide();
+            renderChatroom();
+            $('#username').text('Hi ' + $('#m1').val() + '!');
             $('#messages').scrollTop($('#messages')[0].scrollHeight);
 
-
         } else if (from == "fromLogout") {
+
             socket.disconnect();
-            showPageOne();
+            renderLoginPage();
 
         }
     }
 
-
-    $(function() {
-        showPageOne();
-    })
-
-    function showPageOne() {
-        $('#chatroom').hide();
-        $('#logout').hide();
-        $('#username').hide();
-        $('#login').show();
-        $('#m1').val('');
+    function renderChatroom() {
+        $('#chatroom').show();
+        $('#logout').show();
+        $('#username').show();
+        $('#login').hide();
     }
 
     function prepareChatBubble(emittedAtom) {
-
-        var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        var d = new Date(0);
         d.setUTCMilliseconds(emittedAtom.when);
+
         var when = "<li><div style=\"color:grey; text-align:center; font-size:x-small\">" + d.toString() + "</div>";
         var who = "<div style=\"color:Red; text-align:center; font-size:x-small\">" + emittedAtom.who + "</div>";
         var what = "<div style=\"font-size:small\">" + emittedAtom.what + "</div></li>";
+
         $('#messages').append(when + who + what);
     }
-
-    socket.on('inputMessage', function(msg) {
-        var emittedAtom = JSON.parse(msg.toString());
-        prepareChatBubble(emittedAtom);
-        $('#messages').scrollTop($('#messages')[0].scrollHeight);
-    });
